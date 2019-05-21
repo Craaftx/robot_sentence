a9aba09c9168cc646a16f417f7f23ef22f6e5d28
Craaftx <guilmain.dorian@gmail.com>
2019-05-21 17:50:55 +0200

    :sparkles: Add JSON parser

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,9 @@ let sentenceTheme = "none";
 
 let themeData = ['Western', 'Jeux'];
 let sentenceData = Object.create(null);
-sentenceData.begin = 'sentenceBegin.json';
-sentenceData.body = 'sentenceBody.json';
-sentenceData.end = 'sentenceEnd.json';
+sentenceData.begin = 'sentences_begin.json';
+sentenceData.body = 'sentences_body.json';
+sentenceData.end = 'sentences_end.json';
 
 let frontEnd_Display = document.getElementById("frontend-result");
 
@@ -27,3 +27,27 @@ function resetActionList(htmlSelector, value) {
     }
     actionList[value - 1].classList.add("active");
 }
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+getJSON('../assets/js/data/' + sentenceData.begin,
+function(err, data) {
+  if (err !== null) {
+    console.log('Something went wrong: ' + err + " - " + './data/'+sentenceData.begin);
+  } else {
+    console.log(data.western.items[0].value);
+  }
+});

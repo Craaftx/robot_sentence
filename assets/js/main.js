b16742a1a9e14507506dc8c17ff7b62f7368500f
Craaftx <guilmain.dorian@gmail.com>
2019-05-25 21:45:01 +0200

    :recycle: Work on Naming

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,39 @@
-let sentenceNumber = 0;
-let sentenceTheme = "none";
+// Global vars
+let quoteNumber = 0;
+let quoteTheme = "none";
+let quoteResult = [];
 
+// Data for quote generation
 let themeData = ['Western', 'Jeux'];
-let sentenceData = Object.create(null);
-sentenceData.begin = 'sentences_begin.json';
-sentenceData.body = 'sentences_body.json';
-sentenceData.end = 'sentences_end.json';
+let quoteData = Object.create(null);
+let quotePath = Object.create(null);
+quotePath.begin = 'quote_begin.json';
+quotePath.body = 'quote_body.json';
+quotePath.end = 'quote_end.json';
 
+// Front End "Hooks"
 let frontEnd_Display = document.getElementById("frontend-result");
+let robot_Talk = document.getElementById("robot__quote");
+let robot = document.getElementById("robot");
 
+// Setup robot_Feeling
+let robot_Feelings = ["", "happy", "talking"];
+function setFeeling(value) {  
+  robot.setAttribute('class', 'robot ' + robot_Feelings[value]);
+}
+
+// Setter for Global Vars
 function setNumber(value) {
-    sentenceNumber = value;
+    quoteNumber = value;
     resetActionList("action-list-number", value);
 }
 
 function setTheme(value) {
-    sentenceTheme = themeData[value];
+    quoteTheme = themeData[value];
     resetActionList("action-list-theme", value);
 }
 
+// Control button activation when setter are used
 function resetActionList(htmlSelector, value) {
     var actionList = document.getElementById(htmlSelector).getElementsByClassName('action-button');
     for(var i= 0; i < actionList.length; i++)
@@ -28,6 +43,7 @@ function resetActionList(htmlSelector, value) {
     actionList[value - 1].classList.add("active");
 }
 
+// Get JSON
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
@@ -43,11 +59,48 @@ var getJSON = function(url, callback) {
     xhr.send();
 };
 
-getJSON('../assets/js/data/' + sentenceData.begin,
+// Get Begin of Quote
+getJSON('../assets/js/data/' + quotePath.begin,
 function(err, data) {
   if (err !== null) {
-    console.log('Something went wrong: ' + err + " - " + './data/'+sentenceData.begin);
+    console.log('Something went wrong: ' + err);
   } else {
-    console.log(data.western.items[0].value);
+    quoteData.begin = data;
   }
 });
+
+// Get Body of Quote
+getJSON('../assets/js/data/' + quotePath.body,
+function(err, data) {
+  if (err !== null) {
+    console.log('Something went wrong: ' + err);
+  } else {
+    quoteData.body = data;
+  }
+});
+
+// Get End of Quote
+getJSON('../assets/js/data/' + quotePath.end,
+function(err, data) {
+  if (err !== null) {
+    console.log('Something went wrong: ' + err);
+  } else {
+    quoteData.end = data;
+  }
+});
+
+robot_Talk.innerText = "Salut ! Utilise les commandes à ta droite pour que je puisse t'aider..";
+setFeeling(0);
+/* 
+function generateQuote() {
+  //quoteNumber
+  //quoteTheme
+  for (var i = 0; i < 9; i++) {
+    str = str + i;
+  } 
+}
+
+function displayQuote() {
+
+}
+*/
